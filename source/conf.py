3d6ff2ab6363a5bdba097bc6f11e5f80dcbb481f
JsHu <80608010+jiashhu@users.noreply.github.com>
2024-12-23 03:44:46 +0800

    Update conf.py

--- source/conf.py
+++ source/conf.py
@@ -33,6 +33,8 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
+# conf.py
+html_baseurl = 'https://<your-username>.github.io/<your-repository-name>/'
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
 
